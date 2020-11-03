a90166cf2984aca58284d60b2b4e4512b14a9362
robhettrick <robert@aucourant.co.uk>
2020-11-03 00:00:48 +0000

    Set http_prefix

--- config.rb
+++ config.rb
@@ -1,3 +1,12 @@
 require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
+
+configure :build do
+  # Generate relative paths to the repo when deploying to GitHub Pages
+  config[:http_prefix] = '/childrens-social-care-architecture'
+  config[:css_dir] = ""
+  config[:js_dir] = ""
+  # activate :minify_css
+  # activate :minify_javascript
+end
