6e475c8ac63707a333e1985a69554ba6ffaf436f
robhettrick <robert@aucourant.co.uk>
2020-11-03 00:07:42 +0000

    Remove css_dir and js_dir

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ GovukTechDocs.configure(self)
 configure :build do
   # Generate relative paths to the repo when deploying to GitHub Pages
   config[:http_prefix] = '/childrens-social-care-architecture'
-  config[:css_dir] = ""
-  config[:js_dir] = ""
+  #config[:css_dir] = ""
+  #config[:js_dir] = ""
   # activate :minify_css
   # activate :minify_javascript
 end
